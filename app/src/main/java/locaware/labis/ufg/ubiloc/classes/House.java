8171dfaf11000a2ea78bc93e0cf7e915d19f03da
Matheus <matheus@casa.com>
2019-02-27 10:05:52 -0300

    Object house created

--- app/src/main/java/locaware/labis/ufg/ubiloc/classes/House.java
+++ app/src/main/java/locaware/labis/ufg/ubiloc/classes/House.java
@@ -3,8 +3,15 @@ package locaware.labis.ufg.ubiloc.classes;
 import java.util.ArrayList;
 
 public class House {
+    private String name;
     private ArrayList<Room> rooms;
+    private int qtdRooms;
     private ArrayList<User> users;
+
+    public House(){
+        rooms = new ArrayList<>();
+        users = new ArrayList<>();
+    }
 
     public ArrayList<Room> getRooms() {
         return rooms;
@@ -20,5 +27,25 @@ public class House {
 
     public void setUsers(ArrayList<User> users) {
         this.users = users;
+    }
+
+    public void setUserAtPosition(int position, User user){
+        this.users.set(position,user);
+    }
+
+    public int getQtdRooms() {
+        return qtdRooms;
+    }
+
+    public void setQtdRooms(int qtdRooms) {
+        this.qtdRooms = qtdRooms;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- app/src/main/java/locaware/labis/ufg/ubiloc/classes/User.java
+++ app/src/main/java/locaware/labis/ufg/ubiloc/classes/User.java
@@ -2,4 +2,12 @@ package locaware.labis.ufg.ubiloc.classes;
 
 public class User {
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
