b37f1c1bb009f2136e2d8e395446838e1b32f944
Matheus <matheus@casa.com>
2019-02-27 09:20:15 -0300

    Test

--- app/src/main/java/locaware/labis/ufg/ubiloc/classes/House.java
+++ app/src/main/java/locaware/labis/ufg/ubiloc/classes/House.java
@@ -5,4 +5,20 @@ import java.util.ArrayList;
 public class House {
     private ArrayList<Room> rooms;
     private ArrayList<User> users;
+
+    public ArrayList<Room> getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(ArrayList<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
 }
