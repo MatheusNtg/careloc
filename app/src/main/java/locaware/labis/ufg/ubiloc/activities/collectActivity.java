c6556ca2d6b87d5204557db5271a1eba39cc6658
Matheus <matheus@casa.com>
2019-03-03 13:58:41 -0300

    Visualização e do dispositivo e seu RSSI feita

--- app/src/main/java/locaware/labis/ufg/ubiloc/activities/collectActivity.java
+++ app/src/main/java/locaware/labis/ufg/ubiloc/activities/collectActivity.java
@@ -1,10 +1,13 @@
 package locaware.labis.ufg.ubiloc.activities;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,8 +22,9 @@ public class collectActivity extends AppCompatActivity {
     BluetoothAdapter bluetoothAdapter;
 
     //Consts
-    public final int REQUEST_ENABLE_BT = 1;
-    public final String TAG = "Debug";
+    private final int REQUEST_ENABLE_BT = 1;
+    private final String TAG = "Debug";
+    private final Context context = this;
 
 
     @Override
@@ -38,19 +42,23 @@ public class collectActivity extends AppCompatActivity {
         enableBT();
         Log.d(TAG, "~ Bluetooth ligado");
 
-        
+        Log.d(TAG, "~ Checking BT permissions");
+        checkBTPermissions();
+
+        Log.d(TAG, "~ Iniciando descoberta de dispositivos");
+        scanLEDevices(bluetoothAdapter);
 
 
 
     }
 
-    public void startBluetoothAdapter(){
+    private void startBluetoothAdapter(){
         // Initializes Bluetooth adapter.
         final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
     }
 
-    public void checkBLEOnDevice(){
+    private void checkBLEOnDevice(){
         // Use this check to determine whether BLE is supported on the device. Then
         // you can selectively disable BLE-related features.
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
@@ -59,12 +67,57 @@ public class collectActivity extends AppCompatActivity {
         }
     }
 
-    public void enableBT(){
+    private void enableBT(){
         // Ensures Bluetooth is available on the device and it is enabled. If not,
         // displays a dialog requesting user permission to enable Bluetooth.
         if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+
+    private void scanLEDevices(BluetoothAdapter bluetoothAdapter){
+        if(bluetoothAdapter.isEnabled()){
+            Log.d(TAG, "~ scanLEDevices: Descoberta iniciada");
+            bluetoothAdapter.startLeScan(leScanCallBack);
+        }else{
+            Toast.makeText(context,"Bluetooth desligado, por favor, ligue o bluetooth", Toast.LENGTH_SHORT);
+        }
+    }
+
+    private BluetoothAdapter.LeScanCallback leScanCallBack = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi, byte[] scanRecord) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(TAG, "Dispositivo encontrado: " + device.getName() + "RSSI: " + rssi);
+                }
+            });
+
+
+        }
+    };
+
+
+    /**
+     * This method is required for all devices running API23+
+     * Android must programmatically check the permissions for bluetooth. Putting the proper permissions
+     * in the manifest is not enough.
+     *
+     * NOTE: This will only execute on versions > LOLLIPOP because it is not needed otherwise.
+     */
+    private void checkBTPermissions() {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+            permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+            if (permissionCheck != 0) {
+
+                this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
+            }
+        }else{
+            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
         }
     }
 }
