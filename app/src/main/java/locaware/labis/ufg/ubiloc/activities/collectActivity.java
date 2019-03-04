833542435241677f92bf25448ec935ef1d042598
Matheus <matheus@casa.com>
2019-03-04 19:43:11 -0300

    Códigos relativos ao Bluetooth movidos para a classe BluetoothUtils

--- app/src/main/java/locaware/labis/ufg/ubiloc/activities/collectActivity.java
+++ app/src/main/java/locaware/labis/ufg/ubiloc/activities/collectActivity.java
@@ -21,7 +21,9 @@ import java.util.Comparator;
 
 import locaware.labis.ufg.ubiloc.R;
 import locaware.labis.ufg.ubiloc.classes.Beacon;
+import locaware.labis.ufg.ubiloc.classes.BluetoothUtils;
 import locaware.labis.ufg.ubiloc.classes.Room;
+import locaware.labis.ufg.ubiloc.classes.Utils;
 import locaware.labis.ufg.ubiloc.innerDatabase.Buffer;
 
 public class collectActivity extends AppCompatActivity {
@@ -31,10 +33,9 @@ public class collectActivity extends AppCompatActivity {
     Button mDetectButton;
 
     //Variables
-    BluetoothAdapter bluetoothAdapter;
+    BluetoothUtils btUtils;
     private Handler handler = new Handler();
     ArrayList<Beacon> devices = new ArrayList<>();
-    Buffer buffer = new Buffer();
     int beaconsQtdDetected = 0;
 
     //Consts
@@ -52,25 +53,20 @@ public class collectActivity extends AppCompatActivity {
 
         //Setting up the activity elemets
         mDetectButton = findViewById(R.id.detectButton);
+        btUtils = new BluetoothUtils(this);
 
 
-        checkBLEOnDevice();
-        Log.d(TAG, "~ O dispositivo aceita BLE");
+        btUtils.checkBLEOnDevice();
 
-        startBluetoothAdapter();
-        Log.d(TAG, "~ Adaptador Bluetooth iniciado");
+        btUtils.enableBT();
 
-        enableBT();
-        Log.d(TAG, "~ Bluetooth ligado");
-
-        Log.d(TAG, "~ Checking BT permissions");
-        checkBTPermissions();
+        btUtils.checkBTPermissions();
 
         mDetectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "~ Iniciando descoberta de dispositivos");
-                scanLEDevices(bluetoothAdapter);
+                scanLEDevices(btUtils.getBluetoothAdapter());
             }
         });
 
@@ -79,31 +75,6 @@ public class collectActivity extends AppCompatActivity {
 
 
     }
-
-    private void startBluetoothAdapter(){
-        // Initializes Bluetooth adapter.
-        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        bluetoothAdapter = bluetoothManager.getAdapter();
-    }
-
-    private void checkBLEOnDevice(){
-        // Use this check to determine whether BLE is supported on the device. Then
-        // you can selectively disable BLE-related features.
-        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            Toast.makeText(this, "BLE não é suportado neste dispositivo", Toast.LENGTH_SHORT).show();
-            finish();
-        }
-    }
-
-    private void enableBT(){
-        // Ensures Bluetooth is available on the device and it is enabled. If not,
-        // displays a dialog requesting user permission to enable Bluetooth.
-        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-    }
-
 
     private void scanLEDevices(final BluetoothAdapter bluetoothAdapter){
         if(bluetoothAdapter.isEnabled()){
@@ -116,10 +87,10 @@ public class collectActivity extends AppCompatActivity {
                     bluetoothAdapter.stopLeScan(leScanCallBack);
 
                     //Gera objeto Beacon de referência a 1m
-                    Beacon referencia = getReferenceBeacon(devices);
+                    Beacon referencia = Utils.getReferenceBeacon(devices);
 
                     //Coloca este beacon de referência no quarto em questão
-                    setReferenceBeacon(referencia,Buffer.getLastHouse().getLastRoom());
+                    Utils.setReferenceBeacon(referencia,Buffer.getLastHouse().getLastRoom());
 
                     String message = "~ MAC: " + referencia.getAddress() + " Média de potência: " + referencia.getRssi();
 
@@ -143,6 +114,9 @@ public class collectActivity extends AppCompatActivity {
         }
     }
 
+
+
+
     private BluetoothAdapter.LeScanCallback leScanCallBack = new BluetoothAdapter.LeScanCallback() {
         @Override
         public void onLeScan(final BluetoothDevice device, final int rssi, byte[] scanRecord) {
@@ -159,65 +133,4 @@ public class collectActivity extends AppCompatActivity {
     };
 
 
-    private Beacon getReferenceBeacon(ArrayList<Beacon> btDevices){
-        Beacon reference = new Beacon();
-
-        int rssiAverage = 0;
-        int count = 0;
-
-        //Ordena o array dos dispositivos descobertos do maior RSSI para o menor
-        devices.sort(new Comparator<Beacon>() {
-            @Override
-            public int compare(Beacon o1, Beacon o2) {
-                return o2.getRssi() - o1.getRssi();
-            }
-        });
-
-        //Obtem o MAC do dispositivo com o maior RSSI
-        String currentAddress = devices.get(0).getAddress();
-
-        //Faz a média dos valores do beacon com maior RSSI
-        for (Beacon b: devices) {
-            if(b.getAddress().equals(currentAddress)){
-                rssiAverage += b.getRssi();
-                count++;
-            }else{
-                break;
-            }
-        }
-
-        rssiAverage = rssiAverage/count;
-
-        reference.setAddress(currentAddress);
-        reference.setRssi(rssiAverage);
-
-        btDevices.clear();
-
-        return reference;
-    }
-
-    public void setReferenceBeacon(Beacon reference, Room room){
-        room.addReferenceBeacon(reference);
-    }
-
-
-    /**
-     * This method is required for all devices running API23+
-     * Android must programmatically check the permissions for bluetooth. Putting the proper permissions
-     * in the manifest is not enough.
-     *
-     * NOTE: This will only execute on versions > LOLLIPOP because it is not needed otherwise.
-     */
-    private void checkBTPermissions() {
-        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
-            int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
-            permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
-            if (permissionCheck != 0) {
-
-                this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001); //Any number
-            }
-        }else{
-            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
-        }
-    }
 }
